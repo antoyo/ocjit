4df0d28a253062a81b06bacdd89ddc9abc4dfba1
Antoni Boucher <bouanto@gmail.com>
2014-11-04 12:19:14 -0500

    Switched from the standard OCaml FFI to Ctypes.

--- examples/ch3_1/src/ch3_1.ml
+++ examples/ch3_1/src/ch3_1.ml
@@ -16,19 +16,22 @@
  * <http://www.gnu.org/licenses/>.
  *)
 
+open Ctypes
 open Ocjit
 
 let () =
     let context = jit_context_create () in
     jit_context_build_start context;
 
-    let params = [| jit_type_sys_int; jit_type_sys_int; jit_type_sys_int |] in
-    let signature = jit_type_create_signature JitAbiCdecl jit_type_sys_int params 1 in
-
+    let params = [ jit_type_sys_int; jit_type_sys_int; jit_type_sys_int ] in
+    let params_array = CArray.of_list jit_type params in
+    let signature = jit_type_create_signature JitAbiCdecl jit_type_sys_int (CArray.start params_array) (List.length params) 1 in
     let jit_function = jit_function_create context signature in
+
     let x = jit_value_get_param jit_function 0 in
     let y = jit_value_get_param jit_function 1 in
     let z = jit_value_get_param jit_function 2 in
+
     let temp1 = jit_insn_mul jit_function x y in
     let temp2 = jit_insn_add jit_function temp1 z in
     jit_insn_return jit_function temp2;
@@ -36,11 +39,13 @@ let () =
     jit_function_compile jit_function;
     jit_context_build_end context;
 
-    let result = ref ((Obj.magic 0) : void) in
-    jit_function_apply jit_function [| ((Obj.magic 3) : void); ((Obj.magic 5) : void); ((Obj.magic 2) : void) |] result;
+    let result = allocate int 0 in
+    let arguments = List.map to_voidp [ allocate int 3; allocate int 5; allocate int 2 ] in
+    let arguments_array = CArray.of_list (ptr void) arguments in
+    jit_function_apply jit_function (CArray.start arguments_array) (to_voidp result);
 
     print_string "mul_add(3, 5, 2) = ";
-    print_int (((Obj.magic !result) : int));
+    print_int (!@ result);
     print_endline "";
 
     jit_context_destroy context
--- ocjit/src/ocjit.ml
+++ ocjit/src/ocjit.ml
@@ -16,41 +16,62 @@
  * <http://www.gnu.org/licenses/>.
  *)
 
+open Ctypes
+open Foreign
+open PosixTypes
+
 type jit_abi = JitAbiCdecl | JitAbiVararg | JitAbiStdcall | JitAbiFastcall
 
-type jit_context
+let int_of_jit_abi = function
+    | JitAbiCdecl -> 0
+    | JitAbiVararg -> 1
+    | JitAbiStdcall -> 2
+    | JitAbiFastcall -> 3
 
-type jit_function
+let jit_abi_of_int = function
+    | 0 -> JitAbiCdecl
+    | 1 -> JitAbiVararg
+    | 2 -> JitAbiStdcall
+    | 3 -> JitAbiFastcall
+    | _ -> invalid_arg "jit_abi"
 
-type jit_type
+let jit_abi = view int ~read: jit_abi_of_int ~write: int_of_jit_abi
 
-type jit_value
+type jit_context = unit ptr
+let jit_context : jit_context typ = ptr void
 
-type void
+type jit_function = unit ptr
+let jit_function : jit_function typ = ptr void
 
-external jit_context_build_end : jit_context -> unit = "jit_context_build_end_c"
+type jit_type = unit ptr
+let jit_type : jit_type typ = ptr void
 
-external jit_context_build_start : jit_context -> unit = "jit_context_build_start_c"
+type jit_value = unit ptr
+let jit_value : jit_value typ = ptr void
 
-external jit_context_create : unit -> jit_context = "jit_context_create_c"
+let jit_context_build_end = foreign "jit_context_build_end" (jit_context @-> returning void)
 
-external jit_context_destroy : jit_context -> unit = "jit_context_destroy_c"
+let jit_context_build_start = foreign "jit_context_build_start" (jit_context @-> returning void)
 
-external jit_function_apply : jit_function -> void array -> void ref -> unit = "jit_function_apply_c"
+let jit_context_create = foreign "jit_context_create" (void @-> returning jit_context)
 
-external jit_function_compile : jit_function -> unit = "jit_function_compile_c"
+let jit_context_destroy = foreign "jit_context_destroy" (jit_context @-> returning void)
 
-external jit_function_create : jit_context -> jit_type -> jit_function = "jit_function_create_c"
+let jit_function_apply = foreign "jit_function_apply" (jit_function @-> ptr (ptr void) @-> ptr void @-> returning void)
 
-external jit_insn_add : jit_function -> jit_value -> jit_value -> jit_value = "jit_insn_add_c"
+let jit_function_compile = foreign "jit_function_compile" (jit_function @-> returning void)
 
-external jit_insn_mul : jit_function -> jit_value -> jit_value -> jit_value = "jit_insn_mul_c"
+let jit_function_create = foreign "jit_function_create" (jit_context @-> jit_type @-> returning jit_function)
 
-external jit_insn_return : jit_function -> jit_value -> unit = "jit_insn_return_c"
+let jit_insn_add = foreign "jit_insn_add" (jit_function @-> jit_value @-> jit_value @-> returning jit_value)
 
-external jit_type_create_signature : jit_abi -> jit_type -> jit_type array -> int -> jit_type = "jit_type_create_signature_c"
+let jit_insn_mul = foreign "jit_insn_mul" (jit_function @-> jit_value @-> jit_value @-> returning jit_value)
 
-external jit_value_get_param : jit_function -> int -> jit_value = "jit_value_get_param_c"
+let jit_insn_return = foreign "jit_insn_return" (jit_function @-> jit_value @-> returning void)
 
-external jit_type_sys_int_c : unit -> jit_type = "jit_type_sys_int_c"
-let jit_type_sys_int = jit_type_sys_int_c ()
+let jit_type_create_signature = foreign "jit_type_create_signature" (jit_abi @-> jit_type @-> ptr jit_type @-> int @-> int @-> returning jit_type)
+
+let jit_type_sys_int' = foreign_value "jit_type_sys_int" jit_type
+let jit_type_sys_int = !@ jit_type_sys_int'
+
+let jit_value_get_param = foreign "jit_value_get_param" (jit_function @-> int @-> returning jit_value)
